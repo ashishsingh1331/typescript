62ad1875b354afb24407794e7cd26f743af3d176
Ashish singh <ashishsingh1331@gmail.com>
2024-08-09 12:15:35 +0530

    Destructure and rest params

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,19 @@
-function printNameAndAge(name: string, options?: { debugMod: boolean }) {
-  console.log(name, options);
+type Options = { debugMod: boolean; indentLevel?: number };
+
+function printNameAndAge(
+  name: string,
+  { debugMod = false, indentLevel = 1 }: Options
+) {
+  console.log(name, debugMod, indentLevel);
 }
 
-printNameAndAge("sdf");
+printNameAndAge("sdf", { debugMod: true, indentLevel: 34 });
+
+// Rest operatoiin
+
+function sum(...nums: number[]) {
+  return; // code to add them
+}
+
+sum(1, 2, 4);
+sum(1, 4);
