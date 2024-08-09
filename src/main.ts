8d7a14792a6ab6b0aec1ed2db6ad4352c2620505
Ashish singh <ashishsingh1331@gmail.com>
2024-08-09 15:41:33 +0530

    Keyof type

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,10 @@ type Person = {
   age: number;
 };
 
-type PersonWithId = Person & { readonly id: string };
-
-const person: Person = { name: "ashish", age: 34 };
-
-const personWithId: PersonWithId = { ...person, id: "Myid" };
-// Interesction is useful when we want to add additional types in existing type
-personWithId.id = "ere";
-
-type NumberArray = readonly number[];
-const numbs: NumberArray = [1, 2, 3];
-numbs.push;
+const age = getValue("age", { name: "Ashish", age: 23 });
+console.log(age);
+// This key we are passing but what should be the type as this need to be dynamic
+// So there is a type call keyof
+function getValue(key: keyof Person, person: Person) {
+  return person[key];
+}
