f6336763d453a0dfff4573a8c93656e7a6e426b8
Ashish singh <ashishsingh1331@gmail.com>
2024-08-09 11:20:54 +0530

    Type vs interface

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,37 @@
-const person: { name: string; age: number; isProgrammer?: boolean } = {
+type Person = {
+  name: string;
+  age: number;
+  isProgrammer?: boolean;
+  friends?: string[];
+  address: {
+    street: string;
+  };
+};
+
+const person: Person = {
   name: "Ashish",
   age: 34,
+  address: {
+    street: "Janakpuri",
+  },
 };
+
+const person2: Person = {
+  name: "Arti",
+  age: 34,
+  address: {
+    street: "Janakpuri",
+  },
+};
+
+// Interface can only be used with objects
+
+interface Animal {
+  name: string;
+  age: number;
+  isProgrammer?: boolean;
+  friends?: string[];
+  address: {
+    street: string;
+  };
+}
