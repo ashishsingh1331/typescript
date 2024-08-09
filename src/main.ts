998a358bf96bde7af94e8884d93e8e9a4541c894
Ashish singh <ashishsingh1331@gmail.com>
2024-08-09 14:22:02 +0530

    Readonly type

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,14 @@ type Person = {
   age: number;
 };
 
-type PersonWithId = Person & { id: string };
+type PersonWithId = Person & { readonly id: string };
 
 const person: Person = { name: "ashish", age: 34 };
 
 const personWithId: PersonWithId = { ...person, id: "Myid" };
 // Interesction is useful when we want to add additional types in existing type
+personWithId.id = "ere";
+
+type NumberArray = readonly number[];
+const numbs: NumberArray = [1, 2, 3];
+numbs.push;
