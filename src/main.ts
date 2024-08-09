2b4681daa6fceb9b85ade3db3dc9404a1fce1bc9
Ashish singh <ashishsingh1331@gmail.com>
2024-08-09 10:57:31 +0530

    Type inference

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,12 @@
-console.log("hi");
+let a: string = "hi";
+// Infer is typescipt is smart enough to infer type of variable based on the value
+a = 2;
+
+const b = 4;
+
+b = 4;
+
+const c = null;
+
+let d: null = null;
+d = undefined;
