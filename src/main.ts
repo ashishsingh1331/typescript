1dab3772db103e5613566969761d1fff61c88a5a
Ashish singh <ashishsingh1331@gmail.com>
2024-08-12 13:33:42 +0530

    Type guard

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
-// Using awaited we can get the type of promise will return
-type Async = Promise<string>;
+// Typeguard is typescript feature by which typescrit do smart work.
+// It check our code flow, and if we have union type for our object property,
+// TS decide the type of property in different part of code
 
-type Value = Awaited<Async>;
+type Todo = {
+  title: string;
+  priority: "High" | "Normal" | "Low";
+  dueDate: Date | string;
+};
 
-async function doSomething() {
-  return 3;
+function extendTodo(todo: Todo) {
+  if (typeof todo.dueDate == "string") {
+    todo.dueDate.length; // Here error disappeared as we added above check
+  }
 }
-
-type New = Awaited<ReturnType<typeof doSomething>>;
