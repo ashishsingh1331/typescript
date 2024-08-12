421978a1ba1982e6ca3052daa3dcf2b02630ba56
Ashish singh <ashishsingh1331@gmail.com>
2024-08-12 11:48:22 +0530

    Omit and pick for creating new

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
-async function wait(duration: number): Promise<string> {
-  return new Promise<string>((resolve) => {
-    setTimeout(() => {
-      resolve("hi");
-    }, duration);
-  });
-}
+// Omit and pick built in types are helpful when we want to create a new type based on available type
+// And we just want to pick only view or omit few properties
 
-await wait(1000).then((value) => console.log(value.length));
+type Todo = {
+  id: string;
+  name: string;
+  status: string;
+  completed: boolean;
+};
+
+// So we ommited id here
+type NewTodo = Omit<Todo, "id">;
