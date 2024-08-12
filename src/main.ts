5b369a7bcdf3dfff57b76792fddd4c32920f01eb
Ashish singh <ashishsingh1331@gmail.com>
2024-08-12 12:07:14 +0530

    Return Type and params

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,10 @@
-// Using partial we can create all properties of object as optional while creting new type
-type Todo = {
-  id: string;
-  name: string;
-  status: string;
-  completed: boolean;
-};
+// ReturnType is used to get the type of data return by function and create a new type
 
-//Partial - all properties are now optional
-type FormTodo = Partial<Todo>;
+function checkLength(a: string, b: number) {
+  return a.length < b;
+}
 
-// Required - all properties are now required but not tested
-type NewFormTodo = Required<Todo>;
+type ReturnTypeOfCheckLength = ReturnType<typeof checkLength>;
+
+// Parameters built in type is helpful to get type of parameters passed to function
+type Params = Parameters<typeof checkLength>;
